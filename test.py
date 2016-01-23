300123a11390d5bfcdbbbe253e411b62910d1b31
Daniel Ng <daniel@medium-one.com>
2016-01-23 13:01:26 -0800

    lily matplot map https://i.gyazo.com/21046884061b17a7930db494b00f62af.png

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import pickle
-import numpy as np
 import gmplot 
+import xlrd
+import pprint
+import csv
 gmap=gmplot.GoogleMapPlotter(47.6097,-122.3331,13)
+cross_reference_xls = xlrd.open_workbook('crossreferencesheet2.xlsx').sheet_by_index(0)
+keys = cross_reference_xls.row(0)
+for x in xrange(len(keys)):
+    keys[x] = str(keys[x])[7:len(str(keys[x]))-1]
 
+cross_reference = {}
+for x in xrange(cross_reference_xls.nrows):
+    print cross_reference_xls.row(x) 
 
-locs=pickle.load(open('./blockfacelocs.p','rb')) # load location data
-clusterlocs=np.asarray(locs.values())
+locs = pickle.load(open('blockfacelocs.p','rb')) # load location data
 lats = []
 lons = []
-for loc in clusterlocs:
-    lats.append(loc[1])
-    lats.append(loc[3])
-    lons.append(loc[0])
-    lons.append(loc[2])
+for id in locs.keys():
+    locs[id] = [(locs[id][1] + locs[id][3])/2, (locs[id][0] + locs[id][2])/2]
+    lats.append(locs[id][0])
+    lons.append(locs[id][1])
 
+pickle.dump(locs, open('avgblockfacelocs.pickle', 'wb'))
 gmap.heatmap(lats, lons)
 gmap.draw("map.html")
